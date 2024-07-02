b479585aca0e8a5da0914ecf98ecbd97cd79769d
renxiao-oss <2541363830@qq.com>
2024-07-02 22:52:21 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
 // index.js
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 const cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(cors());
 
+// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/api/:date_string?', (req, res) => {
-  let dateString = req.params.date_string;
-
-  if (!dateString) {
-    const now = new Date();
-    res.json({ unix: now.getTime(), utc: now.toUTCString() });
-  } else {
-    let date;
-    // Check if the date string is a valid number (timestamp)
-    if (!isNaN(dateString)) {
-      date = new Date(parseInt(dateString));
-    } else {
-      date = new Date(dateString);
-    }
-
-    if (date.toString() === 'Invalid Date') {
-      res.json({ error: 'Invalid Date' });
-    } else {
-      res.json({ unix: date.getTime(), utc: date.toUTCString() });
-    }
-  }
+// API endpoint to return client information
+app.get('/api/whoami', (req, res) => {
+  const ipAddress = req.ip; // Get IP address of client
+  const language = req.headers['accept-language']; // Get preferred language from headers
+  const software = req.headers['user-agent']; // Get user agent from headers
+  
+  // Construct response JSON object
+  const responseObject = {
+    ipaddress: ipAddress,
+    language: language,
+    software: software
+  };
+  
+  // Send response as JSON
+  res.json(responseObject);
 });
 
+// Listen for requests
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
